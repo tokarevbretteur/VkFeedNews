0db824c94d61b6343116f9d45f3ca0754564fd44
Tokarev Vyacheslav <tokarevbretteur@gmail.com>
2023-06-12 18:50:34 +0700

    Initial Commit

---
+++ VkFeedNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkFeedNews
+//
+//  Created by Вячеслав Токарев on 12.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
